2493140465712fd5740117df170540f785868bcf
zhangtao <1037457278@qq.com>
2019-06-19 15:30:44 +0800

    feat:add button css and page test

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div id="app">
-        <nt-button>按钮</nt-button>
+        <nt-button size="large">默认按钮</nt-button>-------
+        <nt-button>默认按钮</nt-button>-------
+        <nt-button size="small">默认按钮</nt-button>
+        <br>
+        <nt-button type="primary" :disabled="false">百搭按钮</nt-button>------
+        <nt-button type="danger" size="small" :long="true">百搭按钮</nt-button>------
+        <nt-button type="warn" @click="query" :disabled="false">查询</nt-button>------
     </div>
 </template>
 
@@ -8,6 +14,12 @@
 export default {
     name: "app",
     components: {
+    },
+    methods: {
+        query(e) {
+            // eslint-disable-next-line no-console
+            console.log(e);
+        }
     }
 };
 </script>
