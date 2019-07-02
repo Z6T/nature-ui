44f6a979a403e1a6e0c7795f6372e4ad6c8bce43
Z6T <1037457278@qq.com>
2019-07-02 18:58:26 +0800

    feat:完善了transfer点击按钮和复选框时的disable状态

--- packages/transfer/frame.vue
+++ packages/transfer/frame.vue
@@ -2,11 +2,10 @@
     <div class="nt-transper__frame">
         <div class="nt-transper__frame__header">{{title}}</div>
         <div class="nt-transper__frame__body">
-            <div
-                class="nt-transper__frame__body__item"
-                v-for="item in frameData"
-                :key="item.key"
-            >{{item.label}}</div>
+            <div class="nt-transper__frame__body__item" v-for="item in frameData" :key="item.key">
+                <input type="checkbox" @click="collectChecked(item.key)" />
+                {{item.label}}
+            </div>
         </div>
     </div>
 </template>
@@ -14,10 +13,32 @@
 <script>
 export default {
     name: 'nt-transfer__frame',
+    data() {
+        return {
+            collections: []
+        }
+    },
     props: {
         frameData: Array,
-        title: String
-    }
+        title: String,
+        type: String // 是左侧列表还是右侧列表
+    },
+    methods: {
+        collectChecked(key) {
+            const cls = this.collections;
+            const isInclude = cls.includes(key);
+            if (!isInclude) {
+                cls.push(key);
+            } else {
+                cls.splice(cls.findIndex(item => item === key), 1)
+            }
+            let flag;
+            cls.length ? flag = true : flag = false;
+            this.type === 'dist' ? this.$parent.toleftDisable = flag : this.$parent.toRightDisable = flag
+
+
+        }
+    },
 }
 </script>
 
--- packages/transfer/index.vue
+++ packages/transfer/index.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="nt-transfer">
-        <Frame title="原始列表" :frameData="srcList"></Frame>
+        <Frame title="原始列表" :frameData="srcList" type="src" ref="src"></Frame>
         <div class="nt-transfer__arrowList absoCenter">
-            <div class="nt-transfer__arrowList__leftArrow absoCenter">&lt;</div>
-            <div class="nt-transfer__arrowList__rightArrow absoCenter">&gt;</div>
+            <div
+                class="nt-transfer__arrowList__leftArrow absoCenter"
+                :class="!toleftDisable?'arrowDisable':''"
+                @click="toLeft"
+            >&lt;</div>
+            <div
+                class="nt-transfer__arrowList__rightArrow absoCenter"
+                :class="!toRightDisable?'arrowDisable':''"
+                @click="toRight"
+            >&gt;</div>
         </div>
-        <Frame title="目标列表" :frameData="distList"></Frame>
+        <Frame title="目标列表" :frameData="distList" type="dist" ref="dist"></Frame>
     </div>
 </template>
 
@@ -21,7 +29,9 @@ export default {
                 { "key": "2", "label": "Content 2", "disabled": true },
                 { "key": "3", "label": "Content 3", "disabled": false }
             ],
-            targetKeys: ["1", "2"]
+            targetKeys: ["1", "2"],
+            toleftDisable: false,
+            toRightDisable: false,
         }
     },
     props: {
@@ -31,13 +41,31 @@ export default {
         srcList() {
             return this.data.filter(item => !this.targetKeys.includes(item.key))
         },
-        distList() {
-            return this.data.filter(item => this.targetKeys.includes(item.key))
-        }
-    },
+        distList: {
+            get() {
+                return this.data.filter(item => this.targetKeys.includes(item.key))
+            }
+        }    },
     components: {
         Frame
-    }
+    },
+    methods: {
+        toLeft() {
+            const distCols = this.$refs.dist.collections;
+            for (let i = 0; i < distCols.length; i++) {
+                const item = distCols[i];
+                this.targetKeys.splice(this.targetKeys.indexOf(item), 1);
+            }
+            this.$refs.dist.collections.length = 0;
+            this.toleftDisable = false;
+        },
+        toRight() {
+            const srcCols = this.$refs.src.collections;
+            this.targetKeys.push(...srcCols);
+            this.$refs.src.collections = []
+            this.toRightDisable = false;
+        }
+    },
 }
 </script>
 
