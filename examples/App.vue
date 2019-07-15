df432999e4f3626232d60913e393a62acc4ca127
Z6T <1037457278@qq.com>
2019-07-15 16:59:16 +0800

    feat:swiper的css以及底部导航显示问题

--- examples/App.vue
+++ examples/App.vue
@@ -8,7 +8,7 @@
             </template>
         </nt-table>
         <hr />
-        <nt-swiper></nt-swiper>
+        <nt-swiper :slideList="slideList"></nt-swiper>
         <hr />
         <nt-transfer :data="datatransfer" :targetKeys="targetKeys" @onChange="handleCHangeTranfsd"></nt-transfer>
 
@@ -55,6 +55,16 @@ export default {
     name: "app",
     data() {
         return {
+            slideList: [{
+                imgSrc: 'https://gma.alicdn.com/bao/uploaded/i4/34532543/O1CN01rm95HU1UejiZiBhxI_!!0-saturn_solar.jpg_250x250.jpg_.webp',
+                clickUrl: "http://baidu.com"
+            }, {
+                imgSrc: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562330773616&di=d29902c5fb8b5f33ec28a67d08b8a68d&imgtype=0&src=http%3A%2F%2Fattachments.gfan.com%2Fforum%2F201608%2F04%2F23490553eyeyyc3yc8mur1.jpg',
+                clickUrl: "http://baidu.com"
+            }, {
+                imgSrc: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562330773989&di=e81bac9a75c76ba10a50c7ae907b89c6&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fitem%2F201307%2F12%2F20130712143815_zXBrZ.jpeg',
+                clickUrl: "http://baidu.com"
+            }],
             datatransfer: [
                 { "key": "1", "label": "Content 1", "disabled": false },
                 { "key": "2", "label": "Content 2", "disabled": true },
--- packages/swiper/index.vue
+++ packages/swiper/index.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="nt-swiper">
         <transition-group tag="ul" name="list" class="nt-swiper__wrapper">
-            <li v-for="(item,i) in slideList" :key="i" v-show="i===curIndex" @mouseenter="stopPlay"
-             @mouseleave="go"
+            <li
+                v-for="(item,i) in slideList"
+                :key="i+'swiper'"
+                v-show="i===curIndex"
+                @mouseenter="stopPlay"
+                @mouseleave="go"
             >
                 <a :href="item.clickUrl">
                     <img :src="item.imgSrc" alt />
@@ -10,10 +14,14 @@
             </li>
         </transition-group>
         <div class="nt-swiper__dots">
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
+            <span
+                class="dot"
+                v-for="(item,i) in slideList.length"
+                :key="i"
+                :class="curIndex === i?'is_active':''"
+                @mouseenter="stopPlay(i)"
+                @mouseleave="go"
+            ></span>
         </div>
     </div>
 </template>
@@ -22,45 +30,37 @@
 import { clearInterval } from 'timers';
 export default {
     name: 'nt-swiper',
+    props: {
+        slideList: Array
+    },
     data() {
         return {
             curIndex: 0,
             timer: '',
-            slideList: [{
-                imgSrc: 'https://gma.alicdn.com/bao/uploaded/i4/34532543/O1CN01rm95HU1UejiZiBhxI_!!0-saturn_solar.jpg_250x250.jpg_.webp',
-                clickUrl: "http://baidu.com"
-            }, {
-                imgSrc: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562330773616&di=d29902c5fb8b5f33ec28a67d08b8a68d&imgtype=0&src=http%3A%2F%2Fattachments.gfan.com%2Fforum%2F201608%2F04%2F23490553eyeyyc3yc8mur1.jpg',
-                clickUrl: "http://baidu.com"
-            }, {
-                imgSrc: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562330773989&di=e81bac9a75c76ba10a50c7ae907b89c6&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fitem%2F201307%2F12%2F20130712143815_zXBrZ.jpeg',
-                clickUrl: "http://baidu.com"
-            }],
-            timer:''
         }
     },
     methods: {
-         go() {
-                this.timer = setInterval(() => {
-                    this.autoPlay()
-                }, 1000)
-            },
+        go() {
+            this.timer = setInterval(() => {
+                this.autoPlay()
+            }, 1000)
+        },
         autoPlay() {
             this.curIndex++;
             if (this.curIndex > this.slideList.length - 1) this.curIndex = 0;
         },
-        stopPlay(){
-            console.log(777);
+        stopPlay(i) {
+            if (typeof i === 'number') this.curIndex = i;
             window.clearInterval(this.timer);
             this.timer = null;
         }
     },
     created() {
-        // this.$nextTick(() => {
-        //     this.timer = setInterval(() => {
-        //         this.autoPlay()
-        //     }, 1000)
-        // })
+        this.$nextTick(() => {
+            this.timer = setInterval(() => {
+                this.autoPlay()
+            }, 1000)
+        })
     },
 }
 </script>
