0c74204038408db1ff6d41ab5d01abb793223ff3
Z6T <1037457278@qq.com>
2019-07-29 13:13:29 +0800

    fix:tree的变动重新计算every,some的逻辑

--- packages/tree/index.vue
+++ packages/tree/index.vue
@@ -1,134 +1,143 @@
 <template >
-  <div class="nt-tree">
-    {{treeData}}
-    <div class="nt-tree__div">
-      <NodeTreeNode :data="data" ref="childNodeWrapper" />
+    <div class="nt-tree">
+        <pre v-html="JSON.stringify(treeData,null,'\t')"></pre>
+        <hr />
+        {{leafLevalObj}}
+        <div class="nt-tree__div">
+            <NodeTreeNode :data="data" ref="childNodeWrapper" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import NodeTreeNode from "./tree-node";
 
 export default {
-  name: "nt-tree",
-  provide() {
-    return {
-      fatherTree: this
-    };
-  },
-  props: {
-    data: Array
-  },
-  data() {
-    return {
-      treeData: [],
-      checkeArr: []
-    };
-  },
-  components: { NodeTreeNode },
-  computed() {},
-  methods: {
-    clickLeaf(item) {
-      this.$emit("node-click", item);
+    name: "nt-tree",
+    provide() {
+        return {
+            fatherTree: this
+        };
     },
-    // 摊平数组，以及数组中子元素的children的数组
-    getFlatChild(childs) {
-      if (!Array.isArray(childs)) return [];
-      const arrres = [];
-      const fun = arr => {
-        for (let index = 0; index < arr.length; index++) {
-          const ele = arr[index];
-          arrres.push(ele);
-          if (ele.children && Array.isArray(ele.children)) {
-            fun(ele.children);
-          }
+    props: {
+        data: Array
+    },
+    data() {
+        return {
+            treeData: [],
+            leafLevalObj: {},
+            checkeArr: []
+        };
+    },
+    components: { NodeTreeNode },
+    computed() { },
+    methods: {
+        clickLeaf(item) {
+            this.$emit("node-click", item);
+        },
+        // 摊平数组，以及数组中子元素的children的数组
+        getFlatChild(childs) {
+            if (!Array.isArray(childs)) return [];
+            const arrres = [];
+            const fun = arr => {
+                for (let index = 0; index < arr.length; index++) {
+                    const ele = arr[index];
+                    arrres.push(ele);
+                    if (ele.children && Array.isArray(ele.children)) {
+                        fun(ele.children);
+                    }
+                }
+            };
+            fun(childs);
+            return arrres;
+        },
+        // 参数是一个数组，判断下边的所有元素是否全选 type就是truefalse
+        isEveryChildChecked(childArr, type) {
+            if (!Array.isArray(childArr) || !childArr.length) return false;
+            return childArr.every(c => typeof c.checked !== undefined && c.checked === type);
+        },
+        // 子元素有一个被选中就返回true
+        isSomeChildChecked(childArr) {
+            if (!Array.isArray(childArr) || !childArr.length) return false;
+            return childArr.some(c => c.checked === true);
+        },
+        // 取到某个数组的最末枝的叶子节点的数组
+        getAllLeafNode(arr, dist = []) {
+            arr.forEach(item => {
+                if (!item.children) {
+                    return dist.push(item)
+                }
+                this.getAllLeafNode(item.children, dist)
+            })
+            return dist
+        },
+        // 如果没有checked属性，加上reactive（响应式）的属性
+        setCheckProp(data, thyp) {
+            const func = arr => {
+                if (Array.isArray(arr)) {
+                    for (let i = 0; i < arr.length; i++) {
+                        const ele = arr[i];
+                        const childs = this.getFlatChild(ele.children);
+                        // 动态为所有节点设置checked属性
+                        if (ele.checked && ele.checked === true && !thyp) {
+                            this.$set(ele, "checked", true);
+                            // 当前节点以及子节点全是checked---简单粗暴
+                            childs.forEach(child => {
+                                this.$set(child, "checked", true);
+                            });
+                        }
+
+                        // 判断当前节点下的所有叶子节点的状态.是全选?全不选?还是只有某些选
+                        let every = null; let everyno = null; let some = null;
+                        let hasChildFlag = ele.children && ele.children.length;
+                        if (hasChildFlag) {
+                            const thisChilds = this.getAllLeafNode(ele.children);
+                            console.log('thisChilds :', ele.label + '的childs', thisChilds);
+                            every = this.isEveryChildChecked(thisChilds, true) // 是否全选
+                            everyno = this.isEveryChildChecked(thisChilds, false) // 是否全不选
+                            // some = this.isSomeChildChecked(thisChilds)
+                            if (every) {
+                                this.$set(ele, "checked", true);
+                            } else if (everyno || !hasChildFlag) {
+                                this.$set(ele, "checked", false);
+                            } else if (hasChildFlag) {
+                                this.$set(ele, "checked", 'some');
+                            }
+                            // console.log('ele every:', ele.label, every);
+                            // console.log('ele everyno:', ele.label, everyno);
+                        } else {
+                            this.$set(ele, "checked", ele.checked)
+                        }
+
+                        func(ele.children);
+                    }
+                }
+                return arr;
+            };
+            this.treeData = func(data)
+            this.treeData.forEach(item => this.leafLevalObj[item.label] = this.getAllLeafNode(item.children))
+
         }
-      };
-      fun(childs);
-      return arrres;
     },
-    // 参数是一个数组，判断下边的所有元素是否全选 type就是truefalse
-    isEveryChildChecked(childArr, type) {
-      if (!Array.isArray(childArr) || !childArr.length) return false;
-      return childArr.every(c => c.checked && c.checked === type);
-    },
-    // 子元素有一个被选中就返回true
-    isSomeChildChecked(childArr) {
-      if (!Array.isArray(childArr) || !childArr.length) return false;
-      return childArr.some(c => c.checked === true);
-    },
-    // 如果没有checked属性，加上reactive（响应式）的属性
-    setCheckProp(data) {
-      const func = arr => {
-        if (Array.isArray(arr)) {
-          for (let i = 0; i < arr.length; i++) {
-            const ele = arr[i];
-            const childs = this.getFlatChild(ele.children);
-            // 动态为所有节点设置checked属性
-            debugger
-            if (ele.checked && ele.checked === true) {
-              // 当前节点以及字节点全是checked
-               this.$set(ele, "every", true);
-                this.$set(ele, "some", false);
-                this.$set(ele, "noevery", false);
-              childs.forEach(child => {
-                this.$set(child, "checked", true);
-                this.$set(child, "every", true);
-                this.$set(child, "some", false);
-                this.$set(child, "noevery", false);
-              });
-            } else {
-              this.$set(ele, "checked", false);
-              const every = this.isEveryChildChecked(childs, true); // 是否全选
-              const some = this.isSomeChildChecked(childs);
-              if (every === true) {
-                this.$set(ele, "every", true);
-                this.$set(ele, "some", false);
-                this.$set(ele, "noevery", false);
-              }
-              // 有一些被选中
-              if (every === false && some === true) {
-                this.$set(ele, "some", true);
-                this.$set(ele, "every", false);
-                this.$set(ele, "noevery", false);
-              }
-              // 全不选
-              if (every === false && some === false) {
-                this.$set(ele, "noevery", true);
-                this.$set(ele, "every", false);
-                this.$set(ele, "some", false);
-              }
-            }
-            func(ele.children);
-          }
+    watch: {
+        data: {
+            handler(val) {
+                // this.setCheckProp(val);
+            },
+            deep: true,
+            immediate: true
+        },
+        treeData: {
+            handler(val) {
+                this.setCheckProp(val, 'init'); // 数据变动不能再走:强制将所有child的checked置为true这个操作,因为点的可能是子元素
+            },
+            deep: true,
+            immediate: true
         }
-        return arr;
-      };
-      this.treeData = func(func(data))
-    }
-  },
-  watch: {
-    data: {
-      handler(val) {
-        this.setCheckProp(val);
-      },
-      deep: true,
-      immediate: true
     },
-    treeData: {
-      handler(val) {
-        console.log("Newval :", val);
-        this.setCheckProp(val);
-        console.log(999);
-      },
-      deep: true,
-      immediate: true
+    created() {
+        this.setCheckProp(this.data);
     }
-  },
-  created() {
-    this.setCheckProp(this.data);
-  }
 };
 </script>
 
--- packages/tree/tree-node.vue
+++ packages/tree/tree-node.vue
@@ -1,139 +1,119 @@
 <template>
-  <ul v-if="data&&data.length" v-show="state_co==='open'||vLeval.length===3">
-    <li
-      v-for="(item,i) in treeData"
-      :key="i"
-      :nt-tree-leval="vLeval+i"
-      :isFolder="!!(item&&item.children)"
-    >
-      <div>
-        <i
-          @click="expandNode($event,i,item)"
-          class="iconfont"
-          :class="item.state==='open'?'icon-Decrease':'icon-Increase'"
-          v-if="item.children"
-        ></i>
-        <input type="checkbox" :checked="item.every||item.checked" @change="handleCheckbox(item)" />
+    <ul v-if="data&&data.length" v-show="state_co==='open'||vLeval.length===3">
+        <li
+            v-for="(item,i) in treeData"
+            :key="i"
+            :nt-tree-leval="vLeval+i"
+            :isFolder="!!(item&&item.children)"
+        >
+            <div>
+                <i
+                    @click="expandNode($event,i,item)"
+                    class="iconfont"
+                    :class="item.state==='open'?'icon-Decrease':'icon-Increase'"
+                    v-if="item.children"
+                ></i>
+                <input
+                    type="checkbox"
+                    :checked="item.checked===true"
+                    @change="handleCheckbox(item)"
+                />
+                {{item.checked}}
+                <span
+                    @click="expandNode($event,i,item)"
+                    :isLastNode="!item.children"
+                    :expand="this_expand"
+                >{{item.label}}</span>
+            </div>
 
-        <span
-          @click="expandNode($event,i,item)"
-          :isLastNode="!item.children"
-          :expand="this_expand"
-        >{{item.label}}</span>
-        every:{{item.every}}------some:{{item.some}}----noevery:{{item.noevery}} ----checked: {{item.checked}}---{{item.id}}
-      </div>
-
-      <tree-node
-        :data="item.children"
-        :boxchecked="curChecked"
-        :state="item.state"
-        :vLeval="vLeval+i+'-'"
-      ></tree-node>
-    </li>
-  </ul>
+            <tree-node :data="item.children" :state="item.state" :vLeval="vLeval+i+'-'"></tree-node>
+        </li>
+    </ul>
 </template>
 
 <script>
-import { log } from "util";
-let draging = null,
-  dragover = null;
 
 export default {
-  name: "TreeNode",
-  inject: ["fatherTree"],
-  data() {
-    return {
-      curCpmponentShow: false,
-      this_expand: false,
-      state_co: this.state, // 当前节点是否关闭或者直接开启
-      curChecked: false
-    };
-  },
-  props: {
-    data: Array,
-    boxchecked: Boolean,
-    vLeval: {
-      type: String,
-      // nt-tree-index
-      default: "nt-"
+    name: "TreeNode",
+    inject: ["fatherTree"],
+    data() {
+        return {
+            curCpmponentShow: false,
+            this_expand: false,
+            state_co: this.state, // 当前节点是否关闭或者直接开启
+            treeData: this.data
+        };
     },
-    state: {
-      type: String,
-      default: "close",
-      validator: s => {
-        return ["open", "close"].indexOf(s) > -1;
-      }
-    }
-  },
-  methods: {
-    getFlatChild(childs) {
-      if (!Array.isArray(childs)) return [];
-      const arrres = [];
-      const fun = arr => {
-        for (let index = 0; index < arr.length; index++) {
-          const ele = arr[index];
-          arrres.push(ele);
-          if (ele.children && Array.isArray(ele.children)) {
-            fun(ele.children);
-          }
+    props: {
+        data: Array,
+        boxchecked: Boolean,
+        vLeval: {
+            type: String,
+            // nt-tree-index
+            default: "nt-"
+        },
+        state: {
+            type: String,
+            default: "close",
+            validator: s => {
+                return ["open", "close"].indexOf(s) > -1;
+            }
         }
-      };
-      fun(childs);
-      return arrres;
     },
-    handleCheckbox(curLiItem) {
-      console.log('curLiItem', curLiItem)
-      const changedCk = !curLiItem.checked;
-      this.setReactiveProp(curLiItem,"checked",changedCk) // 为当前元素赋值checked
-      // curLiItem.checked = changedCk;
-      const childs = curLiItem.children; // 子元素。还有自身的
-      if (childs && childs.length) {
-        const flatChildArr = this.getFlatChild(childs);
-        flatChildArr.forEach(item =>
-          this.setReactiveProp(item, "checked", changedCk)
-        );
-      }
+    methods: {
+        getFlatChild(childs) {
+            if (!Array.isArray(childs)) return [];
+            const arrres = [];
+            const fun = arr => {
+                for (let index = 0; index < arr.length; index++) {
+                    const ele = arr[index];
+                    arrres.push(ele);
+                    if (ele.children && Array.isArray(ele.children)) {
+                        fun(ele.children);
+                    }
+                }
+            };
+            fun(childs);
+            return arrres;
+        },
+        handleCheckbox(curLiItem) {
+            let changedCk = !curLiItem.checked;
+            if (curLiItem.checked === 'some') changedCk = true;
+            this.$set(curLiItem, 'checked', changedCk)
+            // curLiItem.checked = changedCk;
+            const childs = curLiItem.children; // 子元素。还有自身的
+            if (childs && childs.length) {
+                const flatChildArr = this.getFlatChild(childs);
+                flatChildArr.forEach(item => item.checked = changedCk);
+            }
+        },
+        // 为children设置reactive的属性
+        setReactiveProp(obj, key, val) {
+            this.$set(obj, key, val);
+        },
+        expandNode(e, index, item) {
+            if (!this.$children) return;
+            item.state = item.state === "open" ? "close" : "open";
+            this.this_expand = !this.this_expand;
+            const isshow = this.$children[index].state_co;
+            this.$children[index].state_co = isshow === "close" ? "open" : "close";
+            this.fatherTree.clickLeaf({
+                id: item.id,
+                label: item.label,
+                isLeaf: !(item.children && item.children.length !== 0)
+            });
+        }
     },
-    // 为children设置reactive的属性
-    setReactiveProp(obj, key, val) {
-      this.$set(obj, key, val);
-    },
-    expandNode(e, index, item) {
-      if (!this.$children) return;
-      item.state = item.state === "open" ? "close" : "open";
-      this.this_expand = !this.this_expand;
-      const isshow = this.$children[index].state_co;
-      this.$children[index].state_co = isshow === "close" ? "open" : "close";
-      this.fatherTree.clickLeaf({
-        id: item.id,
-        label: item.label,
-        isLeaf: !(item.children && item.children.length !== 0)
-      });
-    },
-    handleCheckbboxClick(i) {
-      // this.treeData[i].curChecked = true;
-      // const curNode = this.treeData[i];
-      // const fn = (curNode) => {
-      //     this.$children && this.$children.forEach(item => {
-      //         item.curChecked = true;
-      //         if (item.children) {
-      //             fn(item.children)
-      //         }
-      //     })
-      // }
-      // fn(curNode)
-    }
-  },
-  watch: {
-    data: {
-      handler(newVal) {
-        this.treeData = newVal;
-      },
+    watch: {
+        data: {
+            handler(newVal) {
+                this.treeData = newVal;
+            },
 
-      deep: true,
-      immediate: true
+            deep: true,
+            immediate: true
+        }
     }
-  }
 };
 </script>
 
